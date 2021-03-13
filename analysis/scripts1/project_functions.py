140b3af6731e91f7e38ac2bbb69f69d94a70940e
byte-food <57513735+byte-food@users.noreply.github.com>
2021-03-13 13:13:27 -0800

    removed -200 values

--- analysis/scripts1/project_functions.py
+++ analysis/scripts1/project_functions.py
@@ -11,27 +11,28 @@ def load_and_process(url_or_path_to_csv_file):
               columns={"PT08.S4(NO2)":"Tungsten Oxide", "PT08.S1(CO)":"Tin Oxide", "C6H6(GT)":"Benzene(GT)", "NO2(GT)":"Tungsten Oxide(GT)", "T": "Temperature", "RH":"Relative Humidity", "AH":"Absolute Humidity", "Time":"Hour of Day"}
               )
       )
-
     # Method Chain 2 (Create new columns, drop others, and do processing)
+    #changing format to fit the graph
+    df1['Temperature'] = df1['Temperature'].str.replace(',','.')
+    df1['Temperature'] = df1['Temperature'].astype(float)
+    df1['Benzene(GT)'] = df1['Benzene(GT)'].str.replace(',','.')
+    df1['Benzene(GT)'] = df1['Benzene(GT)'].astype(float)
+    df1['CO(GT)'] = df1['CO(GT)'].str.replace(',','.')
+    df1['CO(GT)'] = df1['CO(GT)'].astype(float)
+    df1['Relative Humidity'] = df1['Relative Humidity'].str.replace(',','.')
+    df1['Relative Humidity'] = df1['Relative Humidity'].astype(float)
+    df1['Absolute Humidity'] = df1['Absolute Humidity'].str.replace(',','.')
+    df1['Absolute Humidity'] = df1['Absolute Humidity'].astype(float)
 
     df2 = (
           df1
           .drop(columns=["NMHC(GT)", "PT08.S2(NMHC)", "PT08.S5(O3)", "NOx(GT)", "PT08.S3(NOx)", "Unnamed: 15", "Unnamed: 16"])
+          .replace(-200, np.nan)
           .dropna()
          # .assign(...)
       )
 
-    #changing format to fit the graph
-    df2['Temperature'] = df2['Temperature'].str.replace(',','.')
-    df2['Temperature'] = df2['Temperature'].astype(float)
-    df2['Benzene(GT)'] = df2['Benzene(GT)'].str.replace(',','.')
-    df2['Benzene(GT)'] = df2['Benzene(GT)'].astype(float)
-    df2['CO(GT)'] = df2['CO(GT)'].str.replace(',','.')
-    df2['CO(GT)'] = df2['CO(GT)'].astype(float)
-    df2['Relative Humidity'] = df2['Relative Humidity'].str.replace(',','.')
-    df2['Relative Humidity'] = df2['Relative Humidity'].astype(float)
-    df2['Absolute Humidity'] = df2['Absolute Humidity'].str.replace(',','.')
-    df2['Absolute Humidity'] = df2['Absolute Humidity'].astype(float)
+
     # Make sure to return the latest dataframe
       
 
--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from analysis.scripts1 import project_functions
 
 df = project_functions.load_and_process("data\\raw\\AirQualityUCI.csv")
 
-print(df.head())
+print(df.describe())
