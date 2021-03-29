b1668aa99d6a8fec999681f5989feacbe416d579
suyashrs <suyash.rs@alumni.ubc.ca>
2021-03-28 19:59:37 -0700

    EDA Updated

--- analysis/scripts1/.ipynb_checkpoints/main-checkpoint.py
+++
@@ -1,5 +0,0 @@
-import csv
-with open("data\\raw\\AirQualityUCI.csv") as csvfile:
-    reader = csv.DictReader(csvfile, delimiter=';')
-    for row in reader:
-        print(row["Date"]+":"+row["Time"]+row["CO(GT)"])
--- analysis/scripts1/main.py
+++
@@ -1,5 +0,0 @@
-from analysis.scripts1 import project_functions
-
-df = project_functions.load_and_process("data\\raw\\AirQualityUCI.csv")
-
-print(df.describe())
